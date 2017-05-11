7727a739bb88cb1bcce2c1ce4e79ac82acb30b89
isisrin <isisrin@nate.com>
2017-05-11 20:31:08 +0900

    버그수정 ㅠㅠ

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: 'process.env.MICROSOFT_APP_ID',//process.env.MICROSOFT_APP_ID,
-    appPassword: 'process.env.MICROSOFT_APP_PASSWORD'//process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID,//process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD//process.env.MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
 
 server.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`Hello World! ${process.env.MICROSOFT_APP_ID}`);
 });
 
 
