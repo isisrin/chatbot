7ed63ff2b3c3f9c0978a90bca098bcd82dd48a11
isisrin <isisrin@nate.com>
2017-05-11 20:18:38 +0900

    앱 ID, PW 주석 풀음

--- app.js
+++ app.js
@@ -2,25 +2,21 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Setup Restify Server
-var server = restify.createServer(function(req, res) {
-  rew.writeHead(200, {'Content-Type' : 'text/plain'});
-  res.send('Hello World!');
-});
+var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: '',//process.env.MICROSOFT_APP_ID,
-    appPassword: ''//process.env.MICROSOFT_APP_PASSWORD
+    appId: 'process.env.MICROSOFT_APP_ID',//process.env.MICROSOFT_APP_ID,
+    appPassword: 'process.env.MICROSOFT_APP_PASSWORD'//process.env.MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
 
 server.get('/', (req, res) => {
-  //res.writeHead(200, {'Content-Type' : 'text/plain'});
   res.send('Hello World!');
 });
 
