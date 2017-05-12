91499886dfcd54f1f546853951f48c076cb4325d
isisrin <isisrin@nate.com>
2017-05-12 13:56:41 +0900

    원상복구

--- app.js
+++ app.js
@@ -28,35 +28,25 @@ var bot = new builder.UniversalBot(connector);
 //     session.send("You said: %s", session.message.text);
 // });
 
-bot.on('contactRelationUpdate', function (message) {
-    if (message.action === 'add') {
-        var name = message.user ? message.user.name : null;
-        var reply = new builder.Message()
-                .address(message.address)
-                .text("Hello %s... Thanks for adding me.", name || 'there');
-        bot.send(reply);
+bot.dialog('/', [
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('안뇽하시오! %s!', session.userData.name);
     }
-});
+]);
 
-// bot.dialog('/', [
-//     function (session, args, next) {
-//         if (!session.userData.name) {
-//             session.beginDialog('/profile');
-//         } else {
-//             next();
-//         }
-//     },
-//     function (session, results) {
-//         session.send('안뇽하세욤! %s!', session.userData.name);
-//     }
-// ]);
-//
-// bot.dialog('/profile', [
-//     function (session) {
-//         builder.Prompts.text(session, '하이 모두들 안녕? 너는 누구닙?');
-//     },
-//     function (session, results) {
-//         session.userData.name = results.response;
-//         session.endDialog();
-//     }
-// ]);
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, '누구시오!!');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
